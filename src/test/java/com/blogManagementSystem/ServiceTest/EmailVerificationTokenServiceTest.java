aa4075135dd72cc7024908d9e33b7af75c39dfa5
Nihal Singh <nihalsingh2082002@gmail.com>
2026-01-01 16:19:19 +0530

    Added some unit tests

--- src/test/java/com/blogManagementSystem/ServiceTest/EmailVerificationTokenServiceTest.java
+++ src/test/java/com/blogManagementSystem/ServiceTest/EmailVerificationTokenServiceTest.java
@@ -2,6 +2,7 @@ package com.blogManagementSystem.ServiceTest;
 
 import com.blogManagementSystem.entity.EmailVerificationToken;
 import com.blogManagementSystem.entity.User;
+import com.blogManagementSystem.exception.EmailVerificationTokenException;
 import com.blogManagementSystem.repository.EmailVerificationTokenRepository;
 import com.blogManagementSystem.repository.UserRepository;
 import com.blogManagementSystem.service.EmailService;
@@ -9,13 +10,16 @@ import com.blogManagementSystem.service.EmailVerificationTokenService;
 import com.blogManagementSystem.service.EmailVerificationTokenServiceImpl;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.LocalTime;
+import java.util.UUID;
+
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.times;
 
 @ExtendWith(MockitoExtension.class)
 public class EmailVerificationTokenServiceTest {
@@ -28,12 +32,16 @@ public class EmailVerificationTokenServiceTest {
     @Mock
     private  UserRepository userRepository;
 
+    @Mock
+    private EmailVerificationToken emailVerificationToken;
+
     @InjectMocks
     private EmailVerificationTokenServiceImpl emailVerificationTokenService;
 
     private static User user;
     private static String body = "This is the body of mail.";
     private static String subject = "This is the subject of the mail";
+    private static String token;
 
     @BeforeAll
     public static void init(){
@@ -43,6 +51,16 @@ public class EmailVerificationTokenServiceTest {
         user.setFirstName("Nihal");
         user.setLastName("Singh");
 
+        token = UUID.randomUUID().toString();
+    }
+
+    @BeforeEach
+    public void initBeforeEach(){
+        emailVerificationToken = new EmailVerificationToken();
+        emailVerificationToken.setEmailVerificationTokenId(1L);
+        emailVerificationToken.setUser(user);
+        emailVerificationToken.setExpirationTime(LocalTime.now().plusMinutes(15));
+        emailVerificationToken.setVerificationToken(token);
     }
 
     @Test
@@ -70,4 +88,37 @@ public class EmailVerificationTokenServiceTest {
         Assertions.assertEquals(user.getUserEmail(), captureEmailNotificationDetails.getValue());
     }
 
+    @Test
+    public void verifyEmailVerificationToken_shouldVerifyToken(){
+        Mockito.when(emailVerificationTokenRepository.existsByVerificationToken(token)).thenReturn(true);
+        Mockito.when(emailVerificationTokenRepository.findByVerificationToken(token)).thenReturn(emailVerificationToken);
+        Mockito.when(userRepository.save(user)).thenReturn(user);
+        Mockito.doNothing().when(emailVerificationTokenRepository).deleteByVerificationToken(token);
+
+        emailVerificationTokenService.verifyEmailVerificationToken(token);
+    }
+
+    @Test
+    public void verifyEmailVerificationToken_shouldThrowEmailVerificationTokenException(){
+        Mockito.when(emailVerificationTokenRepository.existsByVerificationToken(token)).thenReturn(false);
+
+        EmailVerificationTokenException emailVerificationTokenException = Assertions.assertThrows(EmailVerificationTokenException.class,
+                ()-> emailVerificationTokenService.verifyEmailVerificationToken(token));
+
+        Mockito.verify(emailVerificationTokenRepository, Mockito.never()).findByVerificationToken(token);
+    }
+
+    @Test
+    public void verifyEmailVerificationToken_shouldThrowEmailVerificationTokenExceptionWhenTokenIsExpired(){
+
+        emailVerificationToken.setExpirationTime(LocalTime.now().minusMinutes(20));
+
+        Mockito.when(emailVerificationTokenRepository.existsByVerificationToken(token)).thenReturn(true);
+        Mockito.when(emailVerificationTokenRepository.findByVerificationToken(token)).thenReturn(emailVerificationToken);
+
+        EmailVerificationTokenException emailVerificationTokenException = Assertions.assertThrows(EmailVerificationTokenException.class,
+                ()-> emailVerificationTokenService.verifyEmailVerificationToken(token));
+
+        Mockito.verify(userRepository, Mockito.never()).save(user);
+    }
 }
